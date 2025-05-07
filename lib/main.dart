ad79ddd4886b87ab842f6e65f39cefec5d7d2861
Mustapha <hirehubcompany@gmail.com>
2025-05-07 13:55:16 +0000

    Second Push on Gitub

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,36 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:gocomplaints/screens/landing.dart';
+import 'package:gocomplaints/screens/onboarding%20screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'firebase_options.dart';
 
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MyApp());
+  await Firebase.initializeApp();
+  final prefs = await SharedPreferences.getInstance();
+  final showOnboarding = prefs.getBool('seenOnboarding') ?? false;
+  runApp(MyApp(showOnboarding: showOnboarding));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool showOnboarding;
+  const MyApp({super.key, required this.showOnboarding});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      home: Container(
-        height 100;
+      title: 'GoComplaint',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        useMaterial3: true,
       ),
-
-
+      home: showOnboarding ? LandingPage() : const OnboardingScreen(),
     );
   }
 }
